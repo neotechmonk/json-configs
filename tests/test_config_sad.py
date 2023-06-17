2727b3c363feb2ab71d772762e1dc598f26686de
your_username <your_email@gmail.com>
2023-06-17 16:48:51 +1000

    refact with specific fixtures for malformed files

--- tests/test_config_sad.py
+++ tests/test_config_sad.py
@@ -5,19 +5,24 @@ from src.configs import Configs
 
 
 @pytest.fixture(scope="function")
-def config_file_name():
+def config_file_syntax_error():
     return "./tests/assets/config_malfromed_json_syntax.json"
 
+@pytest.fixture(scope="function")
+def config_file_missing():
+    return "./fake_dir/fake_config.json"
+
+
 @pytest.mark.skip()
-def test_open_missing_config():
+def test_open_missing_config(config_file_missing):
     with pytest.raises(Exception) as exp:
-        Configs("fake_dir/fake_config.json")
+        Configs(config_file_missing)
 
     assert str(exp.value) == "Config file 'fake_dir/fake_config.json' not found."
 
 
-def test_json_syntax_error(config_file_name):
+def test_json_syntax_error(config_file_syntax_error):
     with pytest.raises(ValueError) as exp:
-        config=Configs(config_file_name)
+        config=Configs(config_file_syntax_error)
 
     assert "Invalid JSON format in config file:" in str(exp.value)
