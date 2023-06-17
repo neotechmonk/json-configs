76c73153cea70ff4c0a358bf63a6ea137cac4d70
your_username <your_email@gmail.com>
2023-06-17 13:35:58 +1000

    refactor test - check for type and contents of the root level leaf

--- tests/test_config.py
+++ tests/test_config.py
@@ -15,7 +15,12 @@ def config(config_file_name):
     yield Configs(config_file_name)
 
 def test_is_config_json(config):
-    assert isinstance(config.config,json)
+    assert isinstance(config.config,dict)
 
-def test_read_config(config):
-    assert config.simple_config_text ==  "text_one_liner"
+def test_config_contents(config):
+    assert all([
+        isinstance(config.simple_config_text, str),
+        config.simple_config_text == "text_one_liner"
+    ])
+
+    
